f274741a67c9a3411c83d70c72f02790f370bc84
Chris Porras <cporras@ku.edu>
2016-09-21 10:38:41 -0500

    SlideShow page initialized

---
+++ eecs_448_HTML_Lab/js/slide_show.js
@@ -0,0 +1,14 @@
+//SlideShow.js
+
+var pictures[] = {
+	var img = document.getElementById("cat"),
+	var img2 = document.getElementById("panda"),
+	var img3 = document.getElementById("lemur"),
+	var img4 = document.getElementById("pug"),
+	var img5 = document.getElementById("wolf"),
+	var img6 = document.getElementById("raccoon")
+}
+
+function showCurrentPicture(var index){
+
+}
