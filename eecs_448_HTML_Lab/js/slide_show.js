976a71b833cd016c1b64ad505629ecd943729f35
ChrisPorras <cporras@ku.edu>
2016-09-22 18:59:01 -0500

    Slide Show HTML and JS updated and work

--- eecs_448_HTML_Lab/js/slide_show.js
+++ eecs_448_HTML_Lab/js/slide_show.js
@@ -1,14 +1,35 @@
 //SlideShow.js
+var currentPictureIndex = 0;
 
-var pictures[] = {
-	var img = document.getElementById("cat"),
-	var img2 = document.getElementById("panda"),
-	var img3 = document.getElementById("lemur"),
-	var img4 = document.getElementById("pug"),
-	var img5 = document.getElementById("wolf"),
-	var img6 = document.getElementById("raccoon")
+var pictures = [
+	"img/business_panda.jpg",
+	"img/lemur.jpg",
+	"img/pug.jpg",
+	"img/raccoon.jpg",
+	"img/wolf.jpg"
+];
+
+function nextPicture(){
+	var img = document.getElementById("currentPicture");
+	if(currentPictureIndex== 4){														//if at the end of the array
+		currentPictureIndex = 0;															//go to first index
+	}
+	else {																									//otherwise
+		currentPictureIndex++;																//go to next index
+	}
+	img.src = pictures[currentPictureIndex];							  //set the source to the new index
+	return;
 }
 
-function showCurrentPicture(var index){
-
+//sets the image source to the next one in the array
+function prevPicture(){
+	var img = document.getElementById("currentPicture");
+	if(currentPictureIndex== 0){														//checks if it is the first image
+		currentPictureIndex = 4;															//if first image, go to back of array
+	}
+	else {																									//otherwise
+		currentPictureIndex--;																//go to previous index
+	}
+	img.src = pictures[currentPictureIndex];								//set the sources to new index
+	return;
 }
